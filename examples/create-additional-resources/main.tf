5811dba91153f0c2a5251c37f273605d43e16306
nilic <github@nilic.33mail.com>
2022-08-29 15:14:23 +0200

    update examples

--- examples/create-additional-resources/main.tf
+++ examples/create-additional-resources/main.tf
@@ -1,13 +1,15 @@
 module "argocd" {
   source = "../.."
 
-  argocd_ingress_enabled         = true
-  argocd_project_name            = "my-project"
-  argocd_repository_name         = "gitlab-argo"
-  argocd_repository_url          = "https://gitlab.com/foo/argo.git"
-  argocd_repository_username     = "foo"
-  argocd_repository_password     = "bar123"
-  argocd_application_name        = "root-app"
-  argocd_application_repo_branch = "dev"
-  argocd_application_repo_path   = "argocd/manifests"
+  argocd_ingress_enabled                   = true
+  argocd_admin_password_length             = 16
+  argocd_admin_password_special_characters = "!_"
+  argocd_project_name                      = "my-project"
+  argocd_repository_name                   = "gitlab-argo"
+  argocd_repository_url                    = "https://gitlab.com/foo/argo.git"
+  argocd_repository_username               = "foo"
+  argocd_repository_password               = "bar123"
+  argocd_application_name                  = "root-app"
+  argocd_application_repo_branch           = "dev"
+  argocd_application_repo_path             = "argocd/manifests"
 }
